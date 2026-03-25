8b49b64be6e4c56a6e8a4d2e8463baaa6558f3bd
Jose <joseleal.fr0yo@gmail.com>
2026-03-25 15:00:39 -0400

    feat: Spotify auth working end-to-end

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+const handler = NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "playlist-read-private playlist-read-collaborative",
+        },
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
